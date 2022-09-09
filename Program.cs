dfa97d0940f918e5ec4bc67f6137bc60cde21508
kirill kurenev <cirill@mail.ru>
2022-09-09 22:22:19 +0300

    third task

--- Program.cs
+++ Program.cs
@@ -39,6 +39,17 @@ void PrintArray(double[,] table)
         Console.WriteLine();
     }
 }
+void PrintIntArray(int[,] table)
+{
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            Console.Write(table[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
 
 double[,] FillRealArray(int m, int n)
 {
@@ -56,6 +67,16 @@ double[,] FillRealArray(int m, int n)
     }
     return array;
 }
+int[,] FillArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+            array[i, j] = new Random().Next(1, 100);
+    }
+    return array;
+}
 string CheckElement(int row, int collum, double[,] array)
 {
     try
@@ -66,6 +87,20 @@ string CheckElement(int row, int collum, double[,] array)
     {
         return "такого числа в массиве нет";
     }
+}
+double[] FindAvgCollum(int[,] array)
+{
+    double [] avgCollum = new double[array.GetLength(1)];
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        for (int j = 0; j < array.GetLength(0); j++)
+        {
+            avgCollum[i] += array[j, i];
+        }
+        avgCollum[i] /= array.GetLength(0);
+        Console.WriteLine($"Среднее арифмитическое столбца под номером {i} = {avgCollum[i]}");
+    }
+    return avgCollum;
 }
 Console.WriteLine("Задача 1");
 Console.Write("Введите количество строк двумерного массива ");
@@ -79,4 +114,12 @@ Console.Write("Введите индекс строки ");
 int row = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите индекс столбца ");
 int collum = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(CheckElement(row,collum,mas));
+Console.WriteLine(CheckElement(row, collum, mas));
+Console.WriteLine("Задача 3");
+Console.Write("Введите количество строк двумерного массива ");
+m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество стобцов двумерного массива ");
+n = Convert.ToInt32(Console.ReadLine());
+int[,] arr = FillArray(m, n);
+PrintIntArray(arr);
+double [] avgMas = FindAvgCollum(arr);
